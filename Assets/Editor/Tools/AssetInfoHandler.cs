19299490373a80a9406e18cee54312fc47f24a06
gzhuangzl <gz_huangzl@163.com>
2015-06-19 20:02:00 +0800

    资源信息

---
+++ Assets/Editor/Tools/AssetInfoHandler.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEditor;
+
+namespace Cutomization
+{
+	public class AssetInfoHandler
+	{
+		[MenuItem("Tool/Generate Asset Version Info")]
+		public static void GenerateAssetVersionInfo ()
+		{
+//			SvnClient client = new SvnClient();
+//			SvnTarget target = SvnTarget.FromString("svn://10.18.19.2/projectfun/dev/source/client/PJFun/Assets/Scripts/Game/ViewLib/Common/UIResourceMgr.cs");
+//			target.
+//			client.GetInfo();
+		}
+	}
+}

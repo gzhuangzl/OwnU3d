2d1d4d0ca8558ecd0c8eb2942917cd452fba47df
gzhuangzl <gz_huangzl@163.com>
2015-06-29 18:10:29 +0800

    文件后缀处理

---
+++ Assets/Editor/Tools/FilePostfix.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+
+public class FilePostfix : AssetPostprocessor {
+
+	//最高优先级
+//	public override int GetPostprocessOrder ()
+//	{
+//		return -99999;
+//	}
+
+	void OnPreprocessModel()
+	{
+		Debug.Log(1);
+	}
+	
+	//把新导入的文件名后缀变小字
+	static void OnPostprocessAllAssets(String[] importedAssets ,
+	                            String[] deletedAssets,
+	                            String[] movedAssets,
+	                            String[] movedFromAssetPaths)
+	{
+		foreach(string assetPath in importedAssets)
+		{
+			if(File.Exists(assetPath))
+			{
+				int index = assetPath.LastIndexOf(".");
+				if(index != -1)
+				{
+					string postfix = assetPath.Substring(index);
+					if(postfix != postfix.ToLower())
+					{
+						string newPath = assetPath.Substring(0,index) + postfix.ToLower();
+						FileUtil.MoveFileOrDirectory(assetPath,newPath);
+					}
+				}
+			}
+		}
+	}
+}
